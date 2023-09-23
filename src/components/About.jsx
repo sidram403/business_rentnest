1cd320b3120d597e68aed41f36cc2e1244bd4155
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-09-23 23:17:42 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ function About() {
           <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
           <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
         </div>
-        <div className={`${layout.sectionInfo} text-center`}>
+        <div className={`${layout.sectionInfo} text-left`}>
           <p className={`${styles.paragraph} mt-5`}>
             RentNest is your go-to platform for streamlined and stress-free
             rental experiences. As we work diligently on developing our website,
