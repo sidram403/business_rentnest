6bcf6d09a5510639253e62776119afd13726bb62
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-09-23 23:20:05 +0530

    Update TeamCard.jsx

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function TeamCard({ imgUrl, name, role }) {
   return (
     <>
-      <div className="flex justify-between flex-col px-5 py-5 w-full rounded-[20px] max-w-[400px] team-card">
+      <div className="flex justify-between flex-col px-5 py-5 w-full rounded-[20px] max-w-[360px] team-card">
         <div className="flex flex-row">
           <img
             src={imgUrl}
@@ -11,11 +11,11 @@ function TeamCard({ imgUrl, name, role }) {
             className="w-[120px] h-[120px] rounded-full justify-center mr-5 mt-5 "
           />
           <div className="flex flex-col">
-            <h2 className="font-poppins font-bold text-[22px] leading-[32px] text-gradient text-left my-5 mb-3 border-b-[1px] border-b-[#3F3E45] ">
+            <h2 className="font-poppins font-bold text-[18px] leading-[32px] text-gradient text-left border-b-[1px] border-b-[#3F3E45] sm:text-[22px] ">
               {name}
             </h2>
             <div className="flex flex-col ml-4 mb-5">
-              <p className="font-poppins font-normal text-[18px] leading-[32px] text-white">
+              <p className="font-poppins font-normal text-[16px] leading-[32px] text-white sm:text-[18px]">
                 {role}
               </p>
               <p className="font-poppins font-normal text-[18px] leading-[32px] text-white">
