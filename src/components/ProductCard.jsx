1b2affb80dba1eb99f42496855f9647a75dd4c92
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-09-23 23:19:34 +0530

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function ProductCard({ imgUrl, title, desc }) {
   return (
     <>
-      <div className="flex justify-between flex-col px-5 py-5 w-full rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <div className="flex justify-between flex-col px-5 py-5 w-full rounded-[20px] max-w-[322px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
         <img
           src={imgUrl}
           alt="imgurl"
           className="w-[400px] h-[250px] rounded-[16px]"
         />
-        <h2 className="font-poppins font-bold text-[22px] leading-[32px] text-gradient text-center my-10 ">
+        <h2 className="font-poppins font-bold text-[22px] leading-[32px] text-gradient text-center my-4 ">
           {title}
         </h2>
         <div className="flex flex-row border-b-[1px] border-b-[#3F3E45]">
