3d64a93bc252efcfc1621ccb7f796c11bf27c17c
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-09-23 23:19:50 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,7 +6,7 @@ import TeamCard from "./TeamCard";
 function Team() {
   return (
     <section
-      id="features"
+      id="team"
       className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
       <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
       <div className="w-full flex justify-between items-center text-center md:flex-row flex-col sm:mb-16 mb-6 relative-[1] border-b-[1px] border-b-[#3F3E45] text-gradient">
