f57ff9961a89805a946e6c8b405b6b0458088237
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-09-23 23:18:20 +0530

    Update FeedbackCard.jsx

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { quotes } from "../assets";
 
 const FeedbackCard = ({ content, name }) => (
-  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[336px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
     <img
       src={quotes}
       alt="double_quotes"
