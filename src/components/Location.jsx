81439e66a44c78310e4c3fc5f98a78f765f8842f
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2023-09-23 23:18:57 +0530

    Update Location.jsx

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -15,7 +15,8 @@ function Location() {
           title="map"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d497700.1123005915!2d77.30127814132629!3d12.95379021332858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1670c9b44e6d%3A0xf8dfc3e8517e4fe0!2sBengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1695378433118!5m2!1sen!2sin"
           style={{ border: "0" }}
-          height="350"
+          height="450"
+          width="1024"
           allowfullscreen=""
           loading="lazy"></iframe>
       </div>
